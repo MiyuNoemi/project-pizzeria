013edd0a6352bf7f8548df6f7648c1f4ed6e320b
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-02-14 10:55:30 +0100

    instance for each product

--- src/js/script.js
+++ src/js/script.js
@@ -62,8 +62,16 @@
 
   const app = {
     initMenu: function () {
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
     },
     init: function () {
       const thisApp = this;
@@ -72,7 +80,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
