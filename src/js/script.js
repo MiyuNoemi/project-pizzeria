3825f989bd04f47177f7aaa576ef152954fd6a0e
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-02-16 00:08:29 +0100

    repair code })

--- src/js/script.js
+++ src/js/script.js
@@ -79,53 +79,53 @@
       const trigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       // /* START: click event listener to trigger */
       trigger.addEventListener('click', function () {
-          //     /* prevent default action for event */
-          //     event.preventDefault();
-          //     /* toggle active class on element of thisProduct */
-          //     thisProduct.element.classList.add('active');
-          //     /* find all active products */
-          //     const activeProducts = document.querySelectorAll('active');
-          //     console.log(activeProducts);
-          //     /* START LOOP: for each active product */
-          //     for (let activeProduct of activeProducts) {
-          //       /* START: if the active product isn't the element of thisProduct */
-          //       if (activeProduct !== thisProduct.element) {
-          //         /* remove class active for the active product */
-          //         activeProduct.classList.toggle('active');
-          //         /* END: if the active product isn't the element of thisProduct */
-          //       }
-          //       /* END LOOP: for each active product */
-          //     }
-          //     /* END: click event listener to trigger */
+        /* prevent default action for event */
+        event.preventDefault();
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.add('active');
+        /* find all active products */
+        const activeProducts = document.querySelectorAll('active');
+        console.log(activeProducts);
+        /* START LOOP: for each active product */
+        for (let activeProduct of activeProducts) {
+          /* START: if the active product isn't the element of thisProduct */
+          if (activeProduct !== thisProduct.element) {
+            /* remove class active for the active product */
+            activeProduct.classList.toggle('active');
+            /* END: if the active product isn't the element of thisProduct */
+          }
+          /* END LOOP: for each active product */
         }
-      } //<-- !!!!!ten was ma ze soba problem TÓZ PO NAPISANIU ==trigger.addEventListener==!!!!!!!!!!
+        /* END: click event listener to trigger */
+      });
     }
-
-    const app = {
-      initMenu: function () {
-        const thisApp = this;
-        console.log('thisApp.data:', thisApp.data);
-        for (let productData in thisApp.data.products) {
-          new Product(productData, thisApp.data.products[productData]);
-        }
-      },
-      initData: function () {
-        const thisApp = this;
-
-        thisApp.data = dataSource;
-      },
-      init: function () {
-        const thisApp = this;
-        console.log('*** App starting ***');
-        console.log('thisApp:', thisApp);
-        console.log('classNames:', classNames);
-        console.log('settings:', settings);
-        console.log('templates:', templates);
-
-        thisApp.initMenu();
-        thisApp.dataSource();
-      },
-    };
-
-    app.init();
   }
+
+  const app = {
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+    init: function () {
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+
+      thisApp.initMenu();
+      thisApp.dataSource();
+    },
+  };
+
+  app.init();
+}
