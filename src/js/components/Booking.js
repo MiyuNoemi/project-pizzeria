4c4442fc6a44a7bfff38e220cc95adbef2dc2679
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-03-10 15:50:57 +0100

    Working date/time selection

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -3,6 +3,8 @@ import {
   templates
 } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(bookElem) {
@@ -12,21 +14,22 @@ class Booking {
   }
   render(element) { //argument, który otrzymuje z app.initBooking
     const thisBooking = this;
-    const generatedHTML = templates.bookingWidget(); //generować kod HTML
-    thisBooking.dom = {}; //pusty obiekt
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
   initWidgets() {
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
-
-
 
 export default Booking;
