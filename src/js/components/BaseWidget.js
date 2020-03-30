dc56891515289ed5d10e486eaa067f97ee36dc9e
WeronikaBrzóska <weronikabrzoska97@gmail.com>
2020-03-30 13:38:07 +0200

    Aggregation of data and updating of the HOME

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -1,19 +1,21 @@
 class BaseWidget {
+
   constructor(wrapperElement, initialValue) {
     const thisWidget = this;
 
     thisWidget.dom = {};
     thisWidget.dom.wrapper = wrapperElement;
+
     thisWidget.correctValue = initialValue;
   }
+
   get value() {
     const thisWidget = this;
+
     return thisWidget.correctValue;
   }
-
   set value(value) {
     const thisWidget = this;
-
     const newValue = thisWidget.parseValue(value);
 
     if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
@@ -22,7 +24,11 @@ class BaseWidget {
     }
     thisWidget.renderValue();
   }
+  setValue(value) {
+    const thisWidget = this;
 
+    thisWidget.value = value;
+  }
   parseValue(value) {
     return parseInt(value);
   }
@@ -33,16 +39,13 @@ class BaseWidget {
     const thisWidget = this;
     thisWidget.dom.wrapper.innerHTML = thisWidget.value;
   }
-
   announce() {
     const thisWidget = this;
-
-    const event = new CustomEvent('Updated', {
+    const event = new CustomEvent('updated', {
       bubbles: true
     });
     thisWidget.dom.wrapper.dispatchEvent(event);
   }
 }
-
 
 export default BaseWidget;
